de2689e93476ae11b7e4cb8808f6eee76caff339
Thaddeus Cosley <cosleytg@s.dcsdk12.org>
2023-08-15 16:00:11 -0600

    Added page 2

--- scripts/app.js
+++ scripts/app.js
@@ -3,5 +3,5 @@
 //@ts-ignore canvas is an HTMLCanvasElement
 const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = 800;
-canvas.height = 600;
+canvas.width = 1400;
+canvas.height = 830;
